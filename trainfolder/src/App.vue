8ff7310e435fee6b3d2a9613a4353e55be1cbadb
potokenjoyer <potokenjoyer@gmail.com>
2024-08-30 16:33:08 +0300

    add input +buttonAdd

--- trainfolder/src/App.vue
+++ trainfolder/src/App.vue
@@ -7,6 +7,11 @@
         <button @click="minus" class="btn danger">-</button>
       </div>
     </div>
+    <div class="form-control">
+      <input type="text" />
+    </div>
+    <button class="btn-add">Добавить</button>
+    <hr />
   </div>
 </template>
 
@@ -27,6 +32,46 @@ const message = reactive("Количество:");
 </script>
 
 <style>
+.btn-add {
+  color: #42b983;
+  position: relative;
+  place-content: center;
+  place-items: center;
+  width: fit-content;
+  border-radius: 99px;
+  letter-spacing: 0.05em;
+  border: 1px solid #42b983;
+  text-decoration: none;
+  text-transform: uppercase;
+  margin-top: 2rem;
+  padding: 0.5rem 1.5rem;
+  white-space: nowrap;
+  font-weight: 700;
+  outline: none;
+  background: #fff;
+  transition: all 0.22s;
+}
+.btn-add:hover {
+  cursor: pointer;
+  opacity: 0.8;
+}
+
+.btn:active {
+  box-shadow: inset 1px 1px 1px rgba(0, 0, 0, 0.3);
+}
+
+.form-control input {
+  margin-top: 2rem;
+  outline: none;
+  border: 2px solid #ccc;
+  display: block;
+  width: 100%;
+  color: #2c3e50;
+  padding: 0.5rem 1.5rem;
+  border-radius: 3px;
+  font-size: 1rem;
+}
+
 * {
   box-sizing: border-box;
 }
@@ -97,9 +142,18 @@ h1 {
   padding-top: 5rem;
 }
 
+.container {
+  width: 50%;
+  margin: 0 auto;
+  max-width: 1000px;
+}
 .card {
   padding: 1rem;
   border-radius: 10px;
   box-shadow: 2px 3px 10px rgba(22, 255, 80, 0.2);
+}
+
+hr {
+  margin: 1rem 0;
 }
 </style>
