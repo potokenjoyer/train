7025ad845d9432101f985f1142cd0758b599255e
potokenjoyer <potokenjoyer@gmail.com>
2024-09-09 18:01:45 +0300

    ...

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,8 @@
 <template>
     <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-      <el-tab-pane label="User" name="first">User</el-tab-pane>
-      <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-      <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-      <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+      <el-tab-pane label="Home" name="first">Home</el-tab-pane>
+      <el-tab-pane label="Archive" name="second">Archive</el-tab-pane>
+      <el-tab-pane label="Trash" name="third">Trash</el-tab-pane>
     </el-tabs>
   </template>
   <script lang="ts" setup>
